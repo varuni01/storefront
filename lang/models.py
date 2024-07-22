23b1147db2374d8a3ae38f9ac001fa8ff9c889c2
varuni01 <50800627+varuni01@users.noreply.github.com>
2024-07-22 19:36:52 +0530

    Delete lang directory

--- lang/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class VisitorCounter(models.Model):
-    count = models.PositiveIntegerField(default=0)
-
-    def __str__(self):
-        return f"Visitors: {self.count}"
